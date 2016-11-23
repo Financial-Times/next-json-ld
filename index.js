be6d1732c7379abac794b527f645ef56bc746a09
Glynn Phillips <glynndominicphillips@gmail.com>
2016-11-23 20:57:56 +0000

    Tidying up file structure

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const schema = require('./main');
-const articleData = require('./fixtures/article') 
-
-schema.newsArticle(articleData).then((schema) => {
-	console.log(schema)
-});
