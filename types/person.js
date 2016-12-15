3161d79deda0ba584b51cbbf2b363d549c9dca98
Glynn Phillips <glynndominicphillips@gmail.com>
2016-12-15 15:19:26 +0000

    Adding two authors heights

--- types/person.js
+++ types/person.js
@@ -36,6 +36,18 @@ module.exports = (person) => {
 	if (person.twitterHandle) {
 		Object.assign(baseSchema, {sameAs: [`https://www.twitter.com/${person.twitterHandle}`]});
 	}
+	
+	// Experiment to see if google pick up our data for heights
+	if (person.apiUrl.match(/7fce0429-54de-31d5-b511-acc9c4914eb2$/)) {
+		Object.assign(baseSchema, {height: '1.778 meters'});
+	}
+	
+	// Geeky experiment to see if google will pick up smoots and convert to a common measurement	
+	// https://en.wikipedia.org/wiki/Smoot
+	if (person.apiUrl.indexOf(/660ae9b2-54ed-30b3-8177-79e38a78543f$/)) {
+		Object.assign(baseSchema, {height: '1.01492537 smoots'});
+	}
+
 
 	Object.assign(baseSchema, {worksFor: organization(ftData)})
 
