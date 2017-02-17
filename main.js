c2ec32ad7e2392b2cf321834b2ad7d70a933ef0a
Matt Hinchliffe <i-like-robots@users.noreply.github.com>
2017-02-17 12:21:36 +0000

    Directly export schema types

--- main.js
+++ main.js
@@ -1,36 +1,17 @@
 'use strict';
 
-const ftData = require('./data/ft');
 const organization = require('./types/organization');
-const articleTransform = require('./types/article');
-const personTransform = require('./types/person');
-const webPageTransform = require('./types/webpage');
-const videoTransform = require('./types/video');
+const ftData = require('./data/ft');
 
 function ft () {
 	return organization(ftData);
 }
 
-function article (content) {
-	return articleTransform(content);
-}
-
-function person (content) {
-	return personTransform(content);
-}
-
-function webPage (content) {
-	return webPageTransform(content);
-}
-
-function video (content) {
-	return videoTransform(content);
-}
-
 module.exports = {
 	ft,
-	newsArticle: article,
-	person,
-	webPage,
-	video
+	organization,
+	newsArticle: require('./types/article'),
+	person: require('./types/person'),
+	webPage: require('./types/webpage'),
+	video: require('./types/video')
 };
