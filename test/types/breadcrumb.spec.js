fa12d2ffed159bb5120581b8be06c6fd37fed937
Juan Sanchez Alcala <102036944+juanSanchezAlcala@users.noreply.github.com>
2022-05-06 09:55:06 +0200

    add tests with less elements or displayConcept in annotations

--- test/types/breadcrumb.spec.js
+++ test/types/breadcrumb.spec.js
@@ -37,6 +37,56 @@ describe('Type: Breadcrumb', function () {
 			}]);
 	});
 
+	it('has correct hierarchy when displayConcept is in annotations', function () {
+		let auxArticle = {...article};
+		auxArticle.annotations = [...auxArticle.annotations , auxArticle.displayConcept];
+		const result = breadcrumb(auxArticle);
+		expect(result.itemListElement).to.deep.equal([
+
+			{
+				'@type': 'ListItem',
+				'name': 'Companies',
+				'item': 'https://www.ft.com/companies',
+				'position': 1,
+
+			},
+			{
+				'@type': 'ListItem',
+				'name': 'European Union',
+				'item': 'https://www.ft.com/european-union',
+				'position': 2,
+			},
+
+			{
+				'@type': 'ListItem',
+				'name': 'EU tech regulation',
+				'item': 'https://www.ft.com/stream/433acaac-f1b9-4925-b80c-4fb312c256d8',
+				'position': 3,
+			}]);
+	});
+
+	it('has correct hierarchy when the annotations available are only one', function () {
+		let auxArticle = {...article};
+		auxArticle.annotations = auxArticle.annotations
+			.filter(item => item.prefLabel === 'Companies').slice(0,1);
+		const result = breadcrumb(auxArticle);
+		expect(result.itemListElement).to.deep.equal([
+
+			{
+				'@type': 'ListItem',
+				'name': 'Companies',
+				'item': 'https://www.ft.com/companies',
+				'position': 1,
+
+			},
+			{
+				'@type': 'ListItem',
+				'name': 'EU tech regulation',
+				'item': 'https://www.ft.com/stream/433acaac-f1b9-4925-b80c-4fb312c256d8',
+				'position': 2,
+			}]);
+	});
+
 	it('content does not have displayConcept', function () {
 		let auxArticle = {...article};
 		auxArticle.displayConcept = null;
